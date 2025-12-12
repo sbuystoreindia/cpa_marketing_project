3386bc9de5c0f07bfa78d1c54a569b76f9c3dbb0
sbuystoreindia <sbuystoreindia@gmail.com>
2025-12-13 01:49:18 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/cpa_marketing_project/",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
